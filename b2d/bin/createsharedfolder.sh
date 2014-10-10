3168db6dda046a8f776a5df85b0c0e693a159a4f
K Sasan <ksasan@rediffmail.com>
2014-10-10 21:47:44 +0000

    create a link to shared folder

--- b2d/bin/createsharedfolder.sh
+++ b2d/bin/createsharedfolder.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-docker run -v /data --name my-data busybox true
+cid=`docker run -v /data --name my-data busybox true`
 docker run --rm -v /usr/local/bin/docker:/docker -v /var/run/docker.sock:/docker.sock svendowideit/samba my-data
+path=`docker inspect ${cid} | grep "mnt.*vfs" | head -1 | tr -s " " | cut -d  " " -f 3 | xargs echo `
+ln -s ${path} ~/data
