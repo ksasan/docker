3744f0daa6d3e173ff713348ba31024c1014f55b
Khelender Sasan <ksasan@gmail.com>
2017-04-20 17:19:21 +0530

    First line is working... added 2 additional lines ...

--- recipes/opencats/mysql.sh
+++ recipes/opencats/mysql.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD 'toor' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql  
-echo "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY PASSWORD 'toor' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql  
+echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'toor' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql  
+echo "GRANT ALL ON *.* TO root@'localhost' IDENTIFIED BY 'toor' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql  
+echo "GRANT ALL ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY 'toor' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql  
