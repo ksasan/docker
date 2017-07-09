b6fe89989be049be3f63d663b2a8db6e6cc05a60
Khelender Sasan <ksasan@gmail.com>
2017-07-10 01:01:32 +0530

    Dockerfile has been refined to 1) Move database operations later on (store in a file if can't be done right now) 2) Split dependent operations 3) comment part that is not validated

--- recipes/virtualx/mysql.sh
+++ recipes/virtualx/mysql.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'toor' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql  
+#echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'toor' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql  
