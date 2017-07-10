532e0a4d6a48aaf3d1316df77d41c52248024aee
Khelender Sasan <ksasan@gmail.com>
2017-07-11 00:39:36 +0530

    virtualx configuration file setup

--- recipes/virtualx/virtxDbSetup.sh
+++ recipes/virtualx/virtxDbSetup.sh
@@ -1,4 +1,5 @@
-#/bin/bash
+#!/bin/bash
+echo "GRANT ALL ON *.* TO virtx@'%' IDENTIFIED BY 'virtx' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql -uroot -ptoor -h db
 source /etc/profile.d/rvm.sh		\
 && cd /var/www/virtualx			\
 && rake db:create RAILS_ENV=production	\
